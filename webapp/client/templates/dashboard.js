34f3fe8c72addcf995343c1b91e713698c4d2071
mokolodi1 <mokolodi1@gmail.com>
2015-08-08 15:18:24 -0700

    Teo knows patterns :)

--- webapp/client/templates/dashboard.js
+++ webapp/client/templates/dashboard.js
@@ -1,30 +1,21 @@
 Template.dashboard.onCreated(function () {
   var instance = this;
 
-  instance.innerTemplate = new ReactiveVar("plusSign");
+  instance.hasClicked = new ReactiveVar(false);
 });
 
 // make a helper to get
 Template.dashboard.helpers({
-  getInnerTemplate: function () {
-    return Template.instance().innerTemplate.get();
+  hasClicked: function () {
+    return Template.instance().hasClicked.get();
   },
-});
-
-Template.dashboard.events({
-  "click .thingy": function (event, instance) {
-    if (instance.innerTemplate.get() ==- "plusSign")
-  }
-});
-
-Template.dashboard.helpers({
-	rooms: function() {
+  rooms: function() {
 		return Rooms.find();
 	},
 });
 
 Template.dashboard.events({
-	'click #add_icon': function(event) {
-		//Switch out template
-	},
+  "click #add_icon": function (event, instance) {
+    instance.hasClicked.set(true);
+  }
 });
