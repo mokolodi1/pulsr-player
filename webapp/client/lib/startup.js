cc8e82312fd2e0309be29f2d43c69125b8e43d39
gokul2005 <gokul-raghuraman@users.noreply.github.com>
2015-08-08 20:15:11 -0700

    Moved code out of startup.js for surefire loading of gapi

--- webapp/client/lib/startup.js
+++
@@ -1,9 +0,0 @@
-googleStartup = function () {
-	gapi.client.setApiKey("AIzaSyDYa_mmkyjEiQw5iMm0oBAwVrh3Iv7hvJM");
-	gapi.client.load("youtube", "v3", function() {
-		// youtube api is ready
-	});
-	
-}
-
-	
