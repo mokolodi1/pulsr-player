83a23372d234461e47ebca4240020ad26d8332ac
gokul2005 <gokul-raghuraman@users.noreply.github.com>
2015-08-08 17:11:11 -0700

    Search results show up in a list

--- webapp/client/templates/room.js
+++ webapp/client/templates/room.js
@@ -2,4 +2,10 @@ Template.room.helpers({
 	searchResults: function () {
 		return searchResults.get();
 	}
-})
+});
+
+Template.searchResult.events({
+	'click .listItem': function(event, instance) {
+		window.addSongToPlaylist(instance.data);
+	}
+});
