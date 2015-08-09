27682bc635cb5ad006b691c8f605cd1fdcc55003
gokul2005 <gokul-raghuraman@users.noreply.github.com>
2015-08-09 00:00:59 -0700

    Lots of changes to the room UI

--- webapp/client/templates/room.js
+++ webapp/client/templates/room.js
@@ -1,3 +1,11 @@
+numberWithCommas = function(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
 Template.room.onCreated(function() {
 
 	var instance = this;
