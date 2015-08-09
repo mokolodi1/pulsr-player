bcfaf576a6cdbb44163954ffe5555ee4a61e24d0
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-08-08 18:06:05 -0700

    Added YouTube player but having autorun issues

--- webapp/client/templates/room.js
+++ webapp/client/templates/room.js
@@ -1,3 +1,25 @@
+Template.room.onCreated(function() {
+	var instance = this;
+
+	new YTPlayer("player", {
+		height: '390',
+		width: '640'
+	});
+
+	this.autorun(function() {
+		console.log("Autorun called");
+		if (this.data) {
+			var songID = this.data.current_song_id;
+			console.log(songID);
+			var yt_id = Meteor.findOne(songId).url;
+			if (yt.ready()) {
+				yt.player.loadVideoById(yt_id);
+			}
+			this.stop();
+		}
+	});
+});
+
 Template.room.helpers({
 	searchResults: function () {
 		return searchResults.get();
